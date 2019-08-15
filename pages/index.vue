86316e1322ff82353a5bcd25eb2cb90b4cc5aa54
Leandro Acquati <leandroacquati@gmail.com>
2019-08-15 00:18:21 -0300

    clean default code

--- pages/index.vue
+++ pages/index.vue
@@ -63,13 +63,9 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  data() {
+    return {}
   }
 }
 </script>
