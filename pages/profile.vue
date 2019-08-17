ff9d957c6e70a13fb5023a89682fee7e637f4af8
Leandro Acquati <leandroacquati@gmail.com>
2019-08-16 22:30:10 -0300

    add clear and search button

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,17 +1,70 @@
 <template>
   <v-layout justify-center>
     <v-flex xs12 md8 lg6>
+      <!-- <v-card>
+        <v-card-title class="headline">
+          Welcome to the Vuetify + Nuxt.js template
+        </v-card-title>
+        <v-card-text>
+          <p>
+            Vuetify is a progressive Material Design component framework for
+            Vue.js. It was designed to empower developers to create amazing
+            applications.
+          </p>
+          <p>
+            For more information on Vuetify, check out the
+            <a href="https://vuetifyjs.com" target="_blank"> documentation </a>.
+          </p>
+          <p>
+            If you have questions, please join the official
+            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">
+              discord </a
+            >.
+          </p>
+          <p>
+            Find a bug? Report it on the github
+            <a
+              href="https://github.com/vuetifyjs/vuetify/issues"
+              target="_blank"
+              title="contribute"
+            >
+              issue board </a
+            >.
+          </p>
+          <p>
+            Thank you for developing with Vuetify and I look forward to bringing
+            more exciting features in the future.
+          </p>
+          <div class="text-xs-right">
+            <em><small>&mdash; John Leider</small></em>
+          </div>
+          <hr class="my-3" />
+          <a href="https://nuxtjs.org/" target="_blank">
+            Nuxt Documentation
+          </a>
+          <br />
+          <a href="https://github.com/nuxt/nuxt.js" target="_blank">
+            Nuxt GitHub
+          </a>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/inspire">
+            Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card> -->
       <v-text-field
         v-model="searchText"
         label="Search Users"
         @keyup.enter="fetchResults"
       >
-        <template slot="append">
-          <v-icon v-if="hasText" @click="clearSearchText">mdi-close</v-icon>
+        <!-- <template slot="append">
+          <v-icon v-if="hasText" @click="clearButton">clear</v-icon>
         </template>
         <template slot="append-outer">
-          <v-icon @click="fetchResults">mdi-magnify</v-icon>
-        </template>
+          <v-icon @click="fetchResults">search</v-icon>
+        </template> -->
       </v-text-field>
       <p>{{ feedback }}</p>
 
@@ -67,6 +120,9 @@ export default {
   computed: {
     hasText() {
       return this.searchText.length
+    },
+    hasResults() {
+      return this.results.length
     }
   },
   watch: {
@@ -82,10 +138,17 @@ export default {
   created() {
     this.debouncedFetchResults = _.debounce(this.fetchResults, 1000)
   },
+  mounted() {
+    // this.$axios
+    //   .$get('https://api.github.com/search/users?q=leandro+acquati+type:user')
+    //   .then((result) => {
+    //     console.log(result)
+    //   })
+    //   .catch((error) => {
+    //     console.log(error)
+    //   })
+  },
   methods: {
-    clearSearchText() {
-      this.searchText = ''
-    },
     fetchResults() {
       if (this.searchText === '') return
       this.$axios
