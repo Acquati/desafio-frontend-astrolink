87cb6b49f0dd49cd276a50774069e29ce0950a47
Leandro Acquati <leandroacquati@gmail.com>
2019-08-18 21:44:28 -0300

    add repositories layout

--- pages/_user.vue
+++ pages/_user.vue
@@ -1,7 +1,7 @@
 <template>
   <v-layout justify-center>
     <v-flex xs12 md10 lg8>
-      <v-card v-if="isDataFetched" light>
+      <v-card v-if="isDataFetched" light class="mb-2">
         <v-list-item three-line>
           <v-list-item-avatar size="80" color="grey" tile>
             <v-img
@@ -64,6 +64,77 @@
               </a>
             </v-row>
           </v-list-item>
+        </v-card-actions>
+      </v-card>
+
+      <v-card>
+        <v-card-title>Cafe Badilico</v-card-title>
+        <v-card-text>
+          <v-row align="center">
+            <v-rating
+              :value="4.5"
+              color="amber"
+              half-increments
+              dense
+              size="14"
+              readonly
+            ></v-rating>
+
+            <div class="grey--text ml-4">4.5 (413)</div>
+          </v-row>
+
+          <div class="my-4 subtitle-1 black--text">
+            $ • Italian, Cafe
+          </div>
+
+          <div>
+            Small plates, salads & sandwiches an inteimate setting with 12
+            indoor seats plus patio seating.
+          </div>
+        </v-card-text>
+
+        <v-divider class="mx-4"></v-divider>
+
+        <v-card-text>
+          <div class="title text--primary">Tonight's availability</div>
+          <v-chip-group
+            v-model="selection"
+            active-class="deep-purple accent-4 white--text"
+            column
+          >
+            <v-chip>5:30PM</v-chip>
+            <v-chip>7:30PM</v-chip>
+            <v-chip>8:00PM</v-chip>
+            <v-chip>9:00PM</v-chip>
+          </v-chip-group>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn color="deep-purple accent-4" text @click="reserve">
+            Reserve
+          </v-btn>
+        </v-card-actions>
+
+        <v-divider class="mx-4"></v-divider>
+
+        <v-card-text>
+          <div class="title text--primary">Tonight's availability</div>
+          <v-chip-group
+            v-model="selection"
+            active-class="deep-purple accent-4 white--text"
+            column
+          >
+            <v-chip>5:30PM</v-chip>
+            <v-chip>7:30PM</v-chip>
+            <v-chip>8:00PM</v-chip>
+            <v-chip>9:00PM</v-chip>
+          </v-chip-group>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn color="deep-purple accent-4" text @click="reserve">
+            Reserve
+          </v-btn>
         </v-card-actions>
       </v-card>
       <!-- <p>
